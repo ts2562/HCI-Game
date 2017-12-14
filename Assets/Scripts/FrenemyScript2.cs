12c203399b5d1d17570b555fb352942d97d9bd5f
Timothy Seid <ts2562@nyu.edu>
2017-12-13 22:51:55 -0500

    EndGameBB
    LETS GO

---
+++ Assets/Scripts/FrenemyScript2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrenemyScript2 : MonoBehaviour {
+	bool isLeft = false;
+	Vector3 left = new Vector3(-0.2f,0,0);
+	Vector3 right = new Vector3(0.2f,0,0);
+	Vector3 start;
+	public bool isTouching;
+	float jumpThing = 10.28206f;
+	public GameObject Player;
+	Vector3 EnemyPlayerV;
+	// Use this for initialization
+	void Start () {
+		start = transform.position;		
+		EnemyPlayerV = new Vector3(transform.position.x, transform.position.y + 1f, 0f);
+		isTouching = false;
+	}
+
+	// Update is called once per frame
+	
+	
+	void Update () {
+		//3 - 30 are the x vals
+		EnemyPlayerV = new Vector3(transform.position.x, transform.position.y + 2f, 0f);
+		if(transform.position.x >= 30f){
+			isLeft = true;
+		}
+		if(transform.position.x <= start.x){
+			isLeft = false;
+		}
+		if(isLeft == false)
+			transform.position += right;
+		if(isLeft == true)
+			transform.position += left;
+		if(isTouching == true){
+			Player.transform.position = new Vector3(transform.position.x, Player.transform.position.y, 0f);
+		}
+		if(Player.transform.position.y > jumpThing){
+				isTouching = false;
+			}
+	}
+	void OnCollisionEnter2D(Collision2D col){
+     	if(col.gameObject.tag == "Player"){
+     		//Player.transform.position = new Vector3(transform.position.x, Player.transform.position.y, 0f);
+     		Debug.Log("TEST1");
+     		isTouching = true;
+     	}
+     }
+}
---
+++ Assets/Scripts/FriendlySpike.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlySpike : MonoBehaviour {
+
+	public GameObject Player;
+	Vector3 start = new Vector3(-21.0f, -12.5f, 0);
+	Vector3 playerPos;
+	bool isTouching;
+	// Use this for initialization
+	void Start () {
+		isTouching = false;
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+		playerPos = new Vector3(Player.transform.position.x,Player.transform.position.y,0);
+		if(isTouching == true){
+			float jumpTimer = Time.deltaTime * 120;
+			Vector2 curJumpSpeed = new Vector2 (0, 5* jumpTimer * 2 + 15f);
+			Player.GetComponent<Rigidbody2D> ().velocity = curJumpSpeed;
+			Player.isFalling = true;
+		}
+	}
+*/
+	void OnTriggerEnter2D(Collider2D col){
+     	if(col.gameObject.name == "Player"){
+     		isTouching = true;
+     	}
+     }
+}
